9061e443298dac5ea3b02c9d6f2963e40db3143c
Matthias Fuchs <matthias.fuchs@kps.com>
2017-06-22 22:44:11 +0200

    added threadutil

--- src/at/spot/thready/util/ThreadUtil.java
+++ src/at/spot/thready/util/ThreadUtil.java
@@ -11,4 +11,52 @@ public class ThreadUtil {
 		} catch (final InterruptedException e) {
 		}
 	}
+
+	/**
+	 * Returns the thread id of the thread with the given thread name. If no
+	 * thread is found, -1 is returned.
+	 * 
+	 * @param threadName
+	 * @return
+	 */
+	public static long getThreadIdByName(final String threadName) {
+		final Thread thread = getThreadByName(threadName);
+		final long threadId = (thread != null ? thread.getId() : -1);
+
+		return threadId;
+	}
+
+	/**
+	 * Returns the thread with the given name. If no thread is found, null is
+	 * returned.
+	 * 
+	 * @param threadName
+	 * @return
+	 */
+	public static Thread getThreadByName(final String threadName) {
+		for (final Thread t : Thread.getAllStackTraces().keySet()) {
+			if (t.getName().equals(threadName)) {
+				return t;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Returns the thread with the given thread id. If no thread is found, null
+	 * is returned.
+	 * 
+	 * @param threadId
+	 * @return
+	 */
+	public static Thread getThreadByID(final long threadId) {
+		for (final Thread t : Thread.getAllStackTraces().keySet()) {
+			if (Long.compare(t.getId(), threadId) == 0) {
+				return t;
+			}
+		}
+
+		return null;
+	}
 }
